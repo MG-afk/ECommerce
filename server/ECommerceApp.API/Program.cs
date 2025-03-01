73d3bdcb4d54a4021485f707f1a04a765b65cb00
Michal <m98gabrys@gmail.com>
2025-03-01 12:16:21 +0100

    setup docker files

--- server/ECommerceApp.API/Program.cs
+++ server/ECommerceApp.API/Program.cs
@@ -1,6 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using ECommerceApp.API;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+// Explicitly configure Kestrel to listen on all interfaces
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ListenAnyIP(80);
+});
+
+var startup = new Startup();
+startup.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+startup.Configure(app, app.Environment);
 
 app.Run();
--- server/ECommerceApp.API/Startup.cs
+++ server/ECommerceApp.API/Startup.cs
@@ -2,5 +2,32 @@ namespace ECommerceApp.API;
 
 public class Startup
 {
-    
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Add services to the container
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        });
+    }
 }
